3cb24eb7fef7c045faceccf8ca66d5ee5f298d41
jengle-dev <j.engle.dev@gmail.com>
2023-05-09 18:50:58 -0500

    ok it was the correct develop files pulled down, updated apollo server by mirroring mini project 28 for module 21 MERN

--- server/server.js
+++ server/server.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
 
+const { typeDefs, resolvers } = require('./schemas'); //keep
+const db = require('./config/connection'); //keep
+
+const PORT = process.env.PORT || 3001; //update for Apollo Server
 const app = express();
-const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: false })); //updated to false
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build'))); //keep
 }
 
-app.use(routes);
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })
+})
+};
+
+startApolloServer();
