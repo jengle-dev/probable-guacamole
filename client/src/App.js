37cd7240265117a1a6952bcc44c758753e9c063c
jengle-dev <j.engle.dev@gmail.com>
2023-05-09 18:39:17 -0500

    I think I may have copies the wrong files down but saving before pulling other files so I have the ability to revert.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Apollo Server & Apollo API
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import NotFound from './pages/NotFound';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}></ApolloProvider>
     <Router>
-      <>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
         <Navbar />
         <Routes>
           <Route 
@@ -20,10 +30,10 @@ function App() {
           />
           <Route 
             path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
+            element={<h1 className='display-2'>404: Wrong page!</h1>}
           />
         </Routes>
-      </>
+        </div>
     </Router>
   );
 }
